6c7e6a7e241e0f61b1d6ac5612f7d07b38958e14
Pavel Maslov <dnfanl@gmail.com>
2026-01-06 20:54:59 +0100

    Rolling command for the dice bot

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# import argparse
 import os
 import discord
-
 from dotenv import load_dotenv
-from functions.roll import roll_multiple
 from discord.ext import commands
+from functions.roll import roll_multiple
 
 
 def main():
@@ -15,19 +13,28 @@ def main():
     intents = discord.Intents()
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix="/", intents=intents)
+    bot = Bot(command_prefix="!", intents=intents)
 
     @bot.event
     async def on_ready():
         print(f"{bot.user} has connected to Discord!")
 
+    @bot.tree.command(name="roll", description="Roll dice in NdM format")
+    async def roll(interaction: discord.Interaction, dice: str):
+        await interaction.response.send_message(roll_dice(dice))
+
     bot.run(str(TOKEN))
+
+
+class Bot(commands.Bot):
+    async def setup_hook(self):
+        await self.tree.sync()
 
 
 def roll_dice(dice_to_roll):
     dice_info = dice_to_roll.split("d")
     if not len(dice_info) == 2:
-        raise ValueError("Incorrect dice info")
+        return "Invalid format. Use NdM (e.g. 2d6)"
 
     number = int(dice_info[0]) if dice_info[0] else 1
     dimensions = int(dice_info[1])
